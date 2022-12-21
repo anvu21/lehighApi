7d2bd9a13bc4bca045dc1acdbe76792c101dfb4e
Annie624 <anniepoon49@gmail.com>
2022-12-20 19:11:42 -0500

    sign in button

--- frontend/src/components/Login/index.jsx
+++ frontend/src/components/Login/index.jsx
@@ -1,10 +1,10 @@
-import { useState } from "react";
-import axios from "axios";
-import { Link } from "react-router-dom";
+//import { useState } from "react";
+//import axios from "axios";
+//import { Link } from "react-router-dom";
 import styles from "./styles.module.css";
 
 const Login = () => {
-	const [data, setData] = useState({ username: "", name: "", password: "" });
+/*	const [data, setData] = useState({ username: "", name: "", password: "" });
 	const [error, setError] = useState("");
 
 	const handleChange = ({ currentTarget: input }) => {
@@ -35,55 +35,18 @@ const Login = () => {
 				setError(error.response.data.message);
 			}
 		}
-	};
+	};*/
 
 	return (
-		<div className={styles.login_container}>
-			<div className={styles.login_form_container}>
-				<div className={styles.left}>
-					<form className={styles.form_container} onSubmit={handleSubmit}>
-						<h1>Login to Your Account</h1>
-						<input
-							type="username"
-							placeholder="username"
-							name="username"
-							onChange={handleChange}
-							value={data.username}
-							required
-							className={styles.input}
-						/>
-						<input
-							type="name"
-							placeholder="name"
-							name="name"
-							onChange={handleChange}
-							value={data.name}
-							required
-							className={styles.input}
-						/>
-						<input
-							type="password"
-							placeholder="Password"
-							name="password"
-							onChange={handleChange}
-							value={data.password}
-							required
-							className={styles.input}
-						/>
-						{error && <div className={styles.error_msg}>{error}</div>}
-						<button type="submit" className={styles.green_btn}>
-							Sign In
-						</button>
-					</form>
-				</div>
-				<div className={styles.right}>
-					<h1>New Here ?</h1>
-					<Link to="/signup">
-						<button type="button" className={styles.white_btn}>
-							Sign Up
-						</button>
-					</Link>
-				</div>
+		<div className={styles.login_container}>			
+			<div className={styles.top}>
+				<img src="https://www2.lehigh.edu/sites/www2/files/2019-01/LehighU_official-logo_White.png"  width = "300" alt=""/>
+			</div>
+			<div className={styles.left}>
+				<h1>Login to Your Account</h1>
+				<button type="submit" className={styles.green_btn}>
+					Sign In
+				</button>
 			</div>
 		</div>
 	);
